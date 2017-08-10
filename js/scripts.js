e678c87cf9bfcd333fd9276f7e3ba9e14c7946da
Erik Zakrzewski and Linda Luu <erik@>
2017-08-10 16:50:07 -0700

    full funtionality to the quiz

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(event) {
+  $("form#movie").submit(function(event) {
+  event.preventDefault();
+    var total = 0;
+    total += parseInt($("select#fave").val());
+    total += parseInt($("select#date").val());
+    total += parseInt($("select#choice").val());
+
+    if (total === 31){
+      $(".mixed").show();
+    }
+
+    if (total <= 12){
+      $(".action").show();}
+    else if (total < 41){
+      $(".comedy").show();}
+    else{
+      $(".romance").show();
+
+    }
+
+
+  
+
+  });
+});
